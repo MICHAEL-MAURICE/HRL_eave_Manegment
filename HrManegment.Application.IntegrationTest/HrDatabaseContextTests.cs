6d2b3c5e2ee6b25ca40da606d0a79b666d916603
Michael Maurice <M.Maurice@klickit.co>
2023-11-28 13:08:26 +0200

    add unit Test and Integration test

---
+++ HrManegment.Application.IntegrationTest/HrDatabaseContextTests.cs
@@ -0,0 +1,42 @@
+
+using HR.LeaveManagement.Domain;
+using HrManegment.Persistence.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using Shouldly;
+
+namespace HR.LeaveManagement.Persistence.IntegrationTests
+{
+    public class HrDatabaseContextTests
+    {
+        private HrDatabaseContext _hrDatabaseContext;
+
+        public HrDatabaseContextTests()
+        {
+            var dbOptions = new DbContextOptionsBuilder<HrDatabaseContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString()).Options;
+
+            _hrDatabaseContext = new HrDatabaseContext(dbOptions);
+        }
+
+        [Fact]
+        public async void Save_SetDateCreatedValue()
+        {
+            // Arrange
+            var leaveType = new LeaveType
+            {
+                Id = Guid.NewGuid(),
+                DefaultDays = 10,
+                Name = "Test Vacation"
+            };
+
+            // Act
+            await _hrDatabaseContext.LeaveTypes.AddAsync(leaveType);
+            await _hrDatabaseContext.SaveChangesAsync();
+
+            // Assert
+            leaveType.DateCreated.ShouldNotBeNull();
+        }
+
+       
+    }
+}
